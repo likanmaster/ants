8f44072b218e00da7248201274ee165de70ed85f
likanmaster <38567416+likanmaster@users.noreply.github.com>
2019-10-14 19:06:08 -0300

    Update Ants.java

--- src/ants/Ants.java
+++ src/ants/Ants.java
@@ -11,36 +11,93 @@ public class Ants {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-int semilla=1;
-int tamcolonia=100; //cantidad de hormigas
+       //valores de entrada 
+int semilla=100;
+int tamcolonia=8; //cantidad de hormigas
 int iteraciones=5;
 double alpha= 0.1;
-double q0=0.9;
+double q0=0.5;
 double betha=2.5;
-int tamciudades;
-String ruta="c:/berlin52.tsp.txt";
+int tamciudades=0;
+String ruta="c:/b6.txt";
 
-
+//variables peruanas
 int dimi=dimension(ruta);
 double[] cx=coordenadax(ruta,dimi);
 double[] cy=coordenaday(ruta,dimi);
-    
-double mmdis [][]=matrizdistancia(dimi,cx,cy);
-double mheu[][]=matrizheuristica(mmdis,dimi);
 int []solinicial=RandomizeArray(0,dimi-1); 
 
-int []mejorsolucion=solinicial;   
+//matriz distancia y heuristica
+double mmdis [][]=matrizdistancia(dimi,cx,cy);
+double mheu[][]=matrizheuristica(mmdis,dimi);
+//mejor solucion
+int []mejorsolucion=solinicial;
 double costomejor = costo(mejorsolucion,mmdis);
+
+//matriz de feromonas
 double mferomona [][]=matrizferomona(costomejor,dimi); 
-int coloniai [][]=coloniainicial(dimi,tamcolonia);
+
+
 
        // for (int i = 0; i < mejorsolucion.length; i++) {
         //     System.out.print (" " + mejorsolucion[i]);
       //  }
+ int ciclo=0;
+      while ( ciclo<iteraciones || (costomejor==7544.3659)) {
+         //System.out.println("Esto lo verás una vez");
+         ciclo++;
+         
+         int colonia [][]=coloniainicial(dimi,tamcolonia);
+         
+         for (int i = 0; i < dimi; i++) {//para cada vertice del grafo
+              for (int j = 1; j < tamcolonia; j++) {//para cada hormiga
+               //seleccionar el prox segmento del grafo usando la ec. 1
+                  double nrandom=randomadec(0,1,semilla);
+               //System.out.print (" numero random " + nrandom);
+                  if (nrandom<q0) {//entonces siguiente jo
+                     //usar la ec 1 parte superior
+                      colonia[i][j]=mejorEcuacion1(mferomona,mheu,dimi,betha);
+                     // System.out.print ("es menor ");
+                    
+                  }
+                  else  {
+                      colonia[i][j]=1;//usar la funcion j
+                  }
+                  
+              }
+          
+              for (int j = 0; j < tamcolonia; j++) {//para cada hormiga
+               //Actualizar la feromona en cada segmento según la Ecuación. 4  
+              }
+         
+         }//fin for general
+         
+          if (true) {//si es encontrada una mejor solución en el actual ciclo
+              //Asignar como mejor solución global a la mejor solución encontrada en la iteración actual.
+
+          }// fin if
+         
+         //Actualizar la feromona en los segmento de la mejor solución global según la Ecuación. 3
+          System.out.println (" ++++");
+          for (int i = 0; i < dimi; i++) {
+            for (int j = 0; j < dimi; j++) {
+                 System.out.print (colonia[i][j]+" ");
+            }
+            System.out.println (" ");
+        }
+       
+      }//fin while 
+      
+      
     } 
      
 // funciones o clases
+    public static double randomadec(int a, int b, int semilla){ 
+    Random r = new Random();
+    Double ri = b + ( a - b ) * r.nextDouble(); 
+    return ri;
+   }
+     
     public static int[] RandomizeArray(int a, int b){
 		Random rgen = new Random();   	
 		int size = b-a+1;
@@ -387,5 +444,33 @@ int coloniai [][]=coloniainicial(dimi,tamcolonia);
     int rndInd = (int)(b + ( a - b ) * rnd.nextDouble());
     return rndInd;
    }
+
+ 
+
+    private static int mejorEcuacion1(double[][] mferomona, double[][] mheu,int dimi,double betha) {
+      //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    double max=0;
+    int pos=0;
+    double []vector=new double[dimi];
+        for (int i = 0; i < dimi; i++) {
+            double aux=mferomona[i][0]*Math.pow(mheu[1][0], betha);
+            
+            vector[i]=aux;
+           // System.out.println ("aux "+aux);
+        }
+    //el max será el la posicion del vector
+        for (int i = 0; i < dimi; i++) {
+            if (vector[i]>max) {
+               max=i;
+            }
+        }
+        for (int i = 0; i < dimi; i++) {
+            if (vector[i]==max) {
+                pos=i;
+            }
+        }
+      
+    return pos;
+    }
     	 
 }
